cfb93ff96e5973b96c352d68ba3b74f0e3c9dfa7
Quanta <cyhawkx@gmail.com>
2026-06-17 13:27:42 -0700

    Updated via Script 2026-17-06 13:27

---
+++ copy_scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SRC="/home/quanta/.local/share/Steam/steamapps/common/Dwarf Fortress/dfhack-config"
+DST="/home/quanta/Projects/q_dwarf_fortress_scripts"
+
+cp -a "$SRC/blueprints/." "$DST/blueprints/"
+cp -a "$SRC/orders/." "$DST/orders/"
---
+++ git_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit 1
+
+if [ $# -gt 0 ]; then
+    COMMIT_MSG="$*"
+else
+    COMMIT_MSG="Updated via Script $(date '+%Y-%d-%m %H:%M')"
+fi
+
+git add .
+git commit -m "$COMMIT_MSG"
+git push
