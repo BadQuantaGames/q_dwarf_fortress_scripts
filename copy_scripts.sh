c6461fc328ce24a75ff657b3547d410f07d0351e
Quanta <cyhawkx@gmail.com>
2026-06-17 13:29:00 -0700

    Updated via Script 2026-17-06 13:29

--- copy_scripts.sh
+++ copy_scripts.sh
@@ -5,3 +5,4 @@ DST="/home/quanta/Projects/q_dwarf_fortress_scripts"
 
 cp -a "$SRC/blueprints/." "$DST/blueprints/"
 cp -a "$SRC/orders/." "$DST/orders/"
+cp -a "$SRC/stockpiles/." "$DST/stockpiles/"
